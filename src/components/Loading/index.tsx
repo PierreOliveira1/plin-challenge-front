90c3bedad95b188e0aee80ceffc640e26c06bdac
Pierre Oliveira <pierre.torres445@gmail.com>
2023-09-15 06:02:35 -0300

    feat: crate component loading

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,7 @@
+export function Loading() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="animate-spin rounded-full border-t-4 border-white border-opacity-25 h-12 w-12"></div>
+    </div>
+  );
+}
